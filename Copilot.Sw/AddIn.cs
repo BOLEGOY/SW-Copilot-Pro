e69e9f93f2fad9600d887d9a7fbbc338e6b647a7
BOLEGOY <151459458+BOLEGOY@users.noreply.github.com>
2022-01-16 06:01:56 +0000

    Page template updates

--- Copilot.Sw/AddIn.cs
+++ Copilot.Sw/AddIn.cs
@@ -3,4 +3,7 @@ using Copilot.Sw;
 using Copilot.Sw.Config;
 using Copilot.Sw.Skills;
 using Copilot.Sw.ViewModels;
-usi
+using Copilot.Sw.Views;
+using Microsoft.Extensions.DependencyInjection;
+using MvvmDialogs;
+u
