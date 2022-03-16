e868cbd9e40449f036d0eb99fbdfc20df24ace84
BOLEGOY <151459458+BOLEGOY@users.noreply.github.com>
2022-03-16 08:15:31 +0000

    Fix ui files

--- Copilot.Sw/Config/TextCompletionProvider.cs
+++ Copilot.Sw/Config/TextCompletionProvider.cs
@@ -1,4 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text
+using System.Text.Json;
+
+namespace Copilot.Sw.Config;
+
+public class TextCompletionProvider:ITextCompletionProv
