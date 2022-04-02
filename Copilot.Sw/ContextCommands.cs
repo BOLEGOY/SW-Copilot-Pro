e2f4f38cf51dd168b14528b04681f84936fdc95b
BOLEGOY <151459458+BOLEGOY@users.noreply.github.com>
2022-04-02 12:16:43 +0000

    Add navbar

--- Copilot.Sw/ContextCommands.cs
+++ Copilot.Sw/ContextCommands.cs
@@ -4,4 +4,8 @@ using Xarial.XCad.UI.Commands.Attributes;
 using Xarial.XCad.UI.Commands.Enums;
 
 [Title("SolidWorks Copilot")]
-[Icon(typeof(Resources),nameof(Resources.SolidWorksC
+[Icon(typeof(Resources),nameof(Resources.SolidWorksCopilot))]
+public enum ContextCommands
+{
+    [Title("Copilot")]
+    [Icon(typeof(
