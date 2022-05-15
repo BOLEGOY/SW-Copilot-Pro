9cf75cd0ce02a0db2d2ce92f2b7491df13d0a6d2
BOLEGOY <151459458+BOLEGOY@users.noreply.github.com>
2022-05-15 11:00:41 +0000

    Funceble Test - Autosave

--- Copilot.Sw/Extensions/KernelExtensions.cs
+++ Copilot.Sw/Extensions/KernelExtensions.cs
@@ -14,3 +14,40 @@ public static class KernelExtensions
     {
         if (configs?.Any() != true)
         {
+            return false;
+        }
+
+        kernelConfig.RemoveAllTextCompletionServices();
+        kernelConfig.RemoveAllTextEmbeddingGenerationServices();
+        foreach (var config in configs)
+        {
+            if (config.Type == ServerType.OpenAI)
+            {
+                kernelConfig.AddOpenAITextCompletionService(
+                    config.Name,                       // alias used in the prompt templates' config.json
+                    config.Model,                     // OpenAI Model Name
+                    config.Apikey,            // OpenAI API key
+                    config.Org
+                    );
+                kernelConfig.AddOpenAITextEmbeddingGenerationService(
+                    config.Name,
+                    "text-embedding-ada-002",
+                    config.Apikey,
+                    config.Org
+                    );
+            }
+            else if (config.Type == ServerType.Azure)
+            {
+                kernelConfig.AddAzureTextCompletionService(
+                    config.Name,
+                    config.Model,
+                    config.Apikey,
+                    config.Org
+                    );
+            }
+        }
+        kernelConfig.SetDefaultTextCompletionService(configs.First().Name);
+
+        return true;
+    }
+}
