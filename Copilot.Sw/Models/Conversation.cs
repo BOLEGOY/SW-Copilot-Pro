7ef0fe05c836b64599a6fc6fe8f08b5b51d6bb93
BOLEGOY <151459458+BOLEGOY@users.noreply.github.com>
2022-05-26 11:26:38 +0000

    updated setup file

--- Copilot.Sw/Models/Conversation.cs
+++ Copilot.Sw/Models/Conversation.cs
@@ -54,3 +54,82 @@ public class Conversation:ObservableObject
         {
             Messages.Add(Message.CreateError(result.LastErrorDescription));
             return;
+        }
+
+        //update history
+        var theNewChatExchange = $"Me: {question}\nAI:{result}\n";
+        AddHistory(theNewChatExchange);
+
+        if (result.Variables.Get("plan", out var planValue)
+            && SwPlanModel.TryParse(planValue, out var planModel))
+        {
+            //plan
+            var actionMessage = new ActionAnswerMessage(planModel);
+            Messages.Add(actionMessage);
+        }
+        else
+        {
+            //normal response
+            Messages.Add(Message.CreateAnswer(result.ToString()));
+        }
+    }
+
+    public async Task ChatWithContextAsync(
+        IKernel kernel,
+        ISkillsProvider skillsProvider,
+        string question,
+        CancellationToken cancellationToken)
+    {
+        Variables.Set("input", question);
+
+        Messages.Add(Message.CreateAsk(question));
+
+        var workingContext = ISldWorksExtensions.GetSwCurrentContext();
+
+        kernel.ImportSkill(new DocumentCreatationSkill());
+
+        //Create a SwPlan skill
+        var plan = new SolidWorksPlanSkill(kernel, skillsProvider);
+        var swPlanSkill = kernel.ImportSkill(plan, SolidWorksPlanSkill.Parameters.ChatWithSolidWorks);
+
+        //send
+        var result = await kernel.RunAsync(
+            Variables,
+            cancellationToken: cancellationToken,
+            swPlanSkill[SolidWorksPlanSkill.Parameters.ChatWithSolidWorks]
+            );
+
+        //check error
+        if (result.ErrorOccurred)
+        {
+            Messages.Add(Message.CreateError(result.LastErrorDescription));
+            return;
+        }
+
+        //update history
+        var theNewChatExchange = $"Me: {question}\nAI:{result}\n";
+        AddHistory(theNewChatExchange);
+
+        if (result.Variables.Get("Plan", out var planValue)
+            && SwPlanModel.TryParse(planValue, out var planModel))
+        {
+            //plan
+            var actionMessage = new ActionAnswerMessage(planModel);
+            Messages.Add(actionMessage);
+        }
+        else
+        {
+            //normal response
+            Messages.Add(Message.CreateAnswer(result.ToString()));
+        }
+    }
+    #endregion
+
+    #region Add
+    internal void AddHistory(string theNewChatExchange)
+    {
+        _history += theNewChatExchange;
+        Variables.Set("history", _history);
+    }
+    #endregion
+}
