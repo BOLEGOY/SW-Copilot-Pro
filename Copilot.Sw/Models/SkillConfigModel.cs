7b83c3dab5c2112c3765106cd60a0168bb619d0c
BOLEGOY <151459458+BOLEGOY@users.noreply.github.com>
2022-06-19 17:51:12 +0000

    db schema

--- Copilot.Sw/Models/SkillConfigModel.cs
+++ Copilot.Sw/Models/SkillConfigModel.cs
@@ -1,4 +1,10 @@
 ﻿using Copilot.Sw.Skills;
 using System.Collections.Generic;
 
-namespace Copilot.Sw.Model
+namespace Copilot.Sw.Models;
+
+public class SkillConfigModel
+{
+    public string? Name { get; set; }
+
+    public s
