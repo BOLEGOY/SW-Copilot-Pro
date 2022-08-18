3e28af94694b4502a50ea744f5e82aef2e49a291
BOLEGOY <151459458+BOLEGOY@users.noreply.github.com>
2022-08-18 16:38:46 +0000

    Proxy commit.

--- Copilot.Sw/Properties/Resources.Designer.cs
+++ Copilot.Sw/Properties/Resources.Designer.cs
@@ -40,3 +40,35 @@ namespace Copilot.Sw.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Copilot.Sw.Properties.Resources", typeof(Resources).Assembly);
+                    resourceMan = temp;
+                }
+                return resourceMan;
+            }
+        }
+        
+        /// <summary>
+        ///   重写当前线程的 CurrentUICulture 属性，对
+        ///   使用此强类型资源类的所有资源查找执行重写。
+        /// </summary>
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
+                return resourceCulture;
+            }
+            set {
+                resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap SolidWorksCopilot {
+            get {
+                object obj = ResourceManager.GetObject("SolidWorksCopilot", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+    }
+}
