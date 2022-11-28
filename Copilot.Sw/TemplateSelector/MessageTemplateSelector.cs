8e0c6f9bb4521485c0b6af973614cddcf006d6d6
BOLEGOY <151459458+BOLEGOY@users.noreply.github.com>
2022-11-28 05:38:49 +0000

    Bump version: 0.0.5 → 0.0.6

---
+++ Copilot.Sw/TemplateSelector/MessageTemplateSelector.cs
@@ -0,0 +1,36 @@
+
+﻿using Copilot.Sw.Models;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Copilot.Sw.TemplateSelector;
+
+public class MessageTemplateSelector : DataTemplateSelector
+{
+    public override DataTemplate? SelectTemplate(
+        object item, 
+        DependencyObject container)
+    {
+        if (item is Message message)
+        {
+            return message.MessageType switch
+            {
+                MessageType.Answer => AnswerDataTemplate,
+                MessageType.Ask => AskDataTemplate,
+                MessageType.Error => ErrorDataTemplate,
+                MessageType.ActionMessage => ActionAnswerDataTemplate,
+                _ => null
+            };
+        }
+
+        return null;
+    }
+
+    public DataTemplate? AnswerDataTemplate { get; set; }
+
+    public DataTemplate? ActionAnswerDataTemplate { get; set; }
+
+    public DataTemplate? AskDataTemplate { get; set; }
+
+    public DataTemplate? ErrorDataTemplate {  get; set; }
+}
