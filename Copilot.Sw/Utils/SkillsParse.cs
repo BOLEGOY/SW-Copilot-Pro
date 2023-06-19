be8865655a2d9f300fd769f5b160950cfa0ec73d
BOLEGOY <151459458+BOLEGOY@users.noreply.github.com>
2023-06-19 10:31:27 +0000

    Header fix

--- Copilot.Sw/Utils/SkillsParse.cs
+++ Copilot.Sw/Utils/SkillsParse.cs
@@ -9,3 +9,21 @@ public static class SkillsParse
     {
         output = output.Trim();
         if (int.TryParse(output,out var value))
+        {
+            return (value, string.Empty);
+        }
+        else
+        {
+            if (output.StartsWith("<"))
+            {
+                XmlDocument doc = new();
+                doc.LoadXml(output);
+                return (-1, doc.InnerText);
+            }
+            else
+            {
+                return (-1, output);
+            }
+        }
+    }
+}
---
+++ Copilot.Sw/ViewModels/QuickChatPaneViewModel.cs
@@ -0,0 +1,56 @@
+
+﻿using CommunityToolkit.Mvvm.Input;
+using Copilot.Sw.Config;
+using Copilot.Sw.Models;
+using Copilot.Sw.Skills;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Copilot.Sw.ViewModels;
+
+public partial class QuickChatPaneViewModel : 
+    WPFChatPaneViewModel
+{
+    public QuickChatPaneViewModel(
+            IAddin addin,
+            ITextCompletionProvider textCompletionProvider,
+            ISkillsProvider skillsProvider) :
+            base(addin, textCompletionProvider, skillsProvider)
+    {
+
+    }
+
+    public Action? CloseAction { get; internal set; }
+
+    [RelayCommand]
+    private void Exit()
+    {
+        if (SendCommand.IsRunning)
+        {
+            SendCommand.Cancel();
+        }
+        CloseAction?.Invoke();
+    }
+
+    protected async override Task SendAsync(
+        CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrWhiteSpace(Question))
+        {
+            return;
+        }
+
+        OnPropertyChanged(nameof(HasItem));
+
+        try
+        {
+
+            //check config
+            if (!_configLoadResult)
+            {
+                OpenSettings();
+            }
+
+            if (Kernel == null)
